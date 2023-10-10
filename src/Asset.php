cf6e02e3a0e0d8ecad5628e0478a6c073e7ebbc1
chipslays <gorshkov34@gmail.com>
2023-10-10 17:03:57 +0400

    feat: новые методы

--- src/Asset.php
+++ src/Asset.php
@@ -6,14 +6,24 @@ class Asset
 {
     protected static array $assets = [];
 
+    public static function override(array $assets): void
+    {
+        self::$assets = $assets;
+    }
+
+    public static function merge(array $assets): void
+    {
+        self::$assets = [...self::$assets, ...$assets];
+    }
+
     public static function add(string $alias, string $value): void
     {
         self::$assets[$alias] = $value;
     }
 
-    public static function get(string $alias): ?string
+    public static function get(string $alias, ?string $default = null): ?string
     {
-        return self::$assets[$alias] ?? null;
+        return self::$assets[$alias] ?? $default;
     }
 
     public static function has(string $alias): bool
