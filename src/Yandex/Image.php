43b3a760c15854111f5e537b03cc7d504cdcfe20
chipslays <gorshkov34@gmail.com>
2023-10-11 10:05:46 +0400

    feat: рефактор и новые методы

--- src/Yandex/Image.php
+++ src/Yandex/Image.php
@@ -4,31 +4,40 @@ namespace Alisa\Yandex;
 
 use Alisa\Configuration;
 use Alisa\Exceptions\ImageException;
+use Alisa\Skill;
 use Alisa\Support\Container;
-use CURLFile;
-use CurlHandle;
+use \CURLFile;
+use \CurlHandle;
 
 class Image
 {
-    protected CurlHandle $httpClient;
-
-    protected Configuration $config;
-
     protected string $host = 'https://dialogs.yandex.net';
 
-    protected ?string $token = null;
+    protected ?string $token;
 
-    protected ?string $skillId = null;
+    protected ?string $skillId;
+
+    protected string $path;
+
+    protected CurlHandle $httpClient;
 
     public function __construct()
     {
-        $this->config = Container::getInstance()->make(Configuration::class);
+        $config = Container::getInstance()->make(Configuration::class);
 
-        if (!$this->token = $this->config->get('token')) {
-            throw new ImageException;
+        if (!$this->token = $config->get('token')) {
+            throw new ImageException('Заполните в конфиге OAuth-токен (token)');
         }
 
-        $this->skillId = $this->config->get('skill_id');
+        if (!$this->skillId = $config->get('skill_id')) {
+            throw new ImageException('Заполните в конфиге идентификатор навыка (skill_id)');
+        }
+
+        $this->path = rtrim($config->get('images', sys_get_temp_dir() . '/alisa/' . $this->skillId . '/images'), '\/');
+
+        if (!file_exists($this->path)) {
+            mkdir($this->path, recursive: true);
+        }
 
         $this->httpClient = curl_init();
         curl_setopt($this->httpClient, CURLOPT_RETURNTRANSFER, true);
@@ -36,7 +45,9 @@ class Image
 
     public function status(): array
     {
-        curl_setopt($this->httpClient, CURLOPT_URL, $this->host . '/api/v1/status');
+        $endpoint = $this->host . '/api/v1/status';
+
+        curl_setopt($this->httpClient, CURLOPT_URL, $endpoint);
         curl_setopt($this->httpClient, CURLOPT_HTTPHEADER, [
             'Authorization: OAuth ' . $this->token,
         ]);
@@ -48,7 +59,9 @@ class Image
 
     public function all(): array
     {
-        curl_setopt($this->httpClient, CURLOPT_URL, $this->host . '/api/v1/skills/' . $this->skillId . '/images');
+        $endpoint = $this->host . '/api/v1/skills/' . $this->skillId . '/images';
+
+        curl_setopt($this->httpClient, CURLOPT_URL, $endpoint);
         curl_setopt($this->httpClient, CURLOPT_HTTPHEADER, [
             'Authorization: OAuth ' . $this->token,
         ]);
@@ -60,8 +73,10 @@ class Image
 
     public function delete(string $id): array
     {
-        curl_setopt($this->httpClient, CURLOPT_URL, $this->host . '/api/v1/skills/' . $this->skillId . '/images/' . $id);
-        curl_setopt($this->httpClient, CURLOPT_CUSTOMREQUEST, "DELETE");
+        $endpoint = $this->host . '/api/v1/skills/' . $this->skillId . '/images/' . $id;
+
+        curl_setopt($this->httpClient, CURLOPT_URL, $endpoint);
+        curl_setopt($this->httpClient, CURLOPT_CUSTOMREQUEST, 'DELETE');
         curl_setopt($this->httpClient, CURLOPT_HTTPHEADER, [
             'Authorization: OAuth ' . $this->token,
         ]);
@@ -71,24 +86,62 @@ class Image
         return $this->handle($response);
     }
 
-    public function upload(string $image): array
+    public function upload(string $image, bool $cache = true): ?string
     {
-        if (!$this->skillId) {
-            throw new ImageException('Заполните в конфиге идентификатор навыка (skill_id)');
+        // Пробуем достать картинку из кеша
+        if ($cache && $response = $this->retrieve($image)) {
+            return $response;
         }
 
         if (!file_exists($image)) {
-            return $this->uploadByUrl($image);
+            $response = $this->uploadByUrl($image);
+        } else {
+            $response = $this->uploadByFile($image);
         }
 
-        return $this->uploadByFile($image);
+        if (!isset($response['image']['id'])) {
+            return null;
+        }
+
+        $imageId = $response['image']['id'];
+
+        // Кешируем картинку
+        if ($cache) {
+            file_put_contents($this->path . '/' . md5($image), $imageId, LOCK_EX);
+        }
+
+        return $imageId;
     }
 
-    protected function uploadByUrl(string $url): array
+    public function once(string $image, bool $cache = true): ?string
     {
+        $id = $this->upload($image, $cache);
+
+        if (!$id) {
+            return null;
+        }
+
+        /** @var Skill $skill */
+        $skill = Container::getInstance()->make(Skill::class);
+
+        $skill->onAfterRun(function () use ($id, $image, $cache) {
+            $this->delete($id);
+
+            if ($cache) {
+                $this->forget($image);
+            }
+        });
+
+        return $id;
+    }
+
+    public function uploadByUrl(string $url): array
+    {
+        $endpoint = $this->host . '/api/v1/skills/' . $this->skillId . '/images';
+
         $payload = json_encode(compact('url'));
 
-        curl_setopt($this->httpClient, CURLOPT_URL, $this->host . '/api/v1/skills/' . $this->skillId . '/images');
+        curl_setopt($this->httpClient, CURLOPT_URL, $endpoint);
         curl_setopt($this->httpClient, CURLOPT_POST, true);
         curl_setopt($this->httpClient, CURLOPT_POSTFIELDS, $payload);
         curl_setopt($this->httpClient, CURLOPT_HTTPHEADER, [
@@ -101,11 +154,13 @@ class Image
         return $this->handle($response);
     }
 
-    protected function uploadByFile(string $file): array
+    public function uploadByFile(string $file): array
     {
+        $endpoint = $this->host . '/api/v1/skills/' . $this->skillId . '/images';
+
         $payload = ['file' => new CURLFile($file)];
 
-        curl_setopt($this->httpClient, CURLOPT_URL, $this->host . '/api/v1/skills/' . $this->skillId . '/images');
+        curl_setopt($this->httpClient, CURLOPT_URL, $endpoint);
         curl_setopt($this->httpClient, CURLOPT_POST, true);
         curl_setopt($this->httpClient, CURLOPT_POSTFIELDS, $payload);
         curl_setopt($this->httpClient, CURLOPT_HTTPHEADER, [
@@ -118,7 +173,19 @@ class Image
         return $this->handle($response);
     }
 
-    protected function handle(string $response): array
+    public function retrieve(string $image): ?string
+    {
+        return @file_get_contents($this->path . '/' . md5($image));
+    }
+
+    public function forget(string $image): self
+    {
+        unlink($this->path . '/' . md5($image));
+
+        return $this;
+    }
+
+    protected function handle(string $response, ?string $image = null): array
     {
         return json_decode($response, true);
     }
