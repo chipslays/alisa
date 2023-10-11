92393fa4621bdaa4a08f7912c40889db27253dd8
chipslays <gorshkov34@gmail.com>
2023-10-11 10:35:14 +0400

    feat: add types

--- src/Support/Helpers.php
+++ src/Support/Helpers.php
@@ -2,11 +2,11 @@
 
 namespace Alisa\Support\Helpers;
 
-function plural($n, array $forms) {
+function plural(string|int|float $n, array $forms): string {
     return $n . ' ' . (is_float($n) ? $forms[1] : ($n % 10 == 1 && $n % 100 != 11 ? $forms[0] : ($n % 10 >= 2 && $n % 10 <= 4 && ($n % 100 < 10 || $n % 100 >= 20) ? $forms[1] : $forms[2])));
 }
 
-function array_sort_by_priority($array) {
+function array_sort_by_priority(array $array): array {
     ksort($array);
 
     return call_user_func_array('array_merge', $array);
